2c871a536f40ea591b38435f4bd62a11bcb7902d
when <when@qq.com>
2021-05-16 16:24:06 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,13 @@
+package git;
+
+/**
+ * Git测试
+ * @author w
+ * @create 2021-05-16 16:18
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
